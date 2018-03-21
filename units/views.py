14cef83bcd35d8ea430ee7d6d57d677da11a9196
aapage <alex@collectordash.com>
2018-03-20 22:54:37 -0400

    initial commit, django project

---
+++ units/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+def test_view(request):
+    input_string = request.GET.get('units', "")
+
+    # return HttpResponse(input_string)
+    return JsonResponse({'units': input_string})
